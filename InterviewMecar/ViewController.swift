4ca37b05fafe3be4844d4e4328659d6db9264e27
Thanh Bui <thanhbpc@imt-soft.com>
2021-03-19 10:39:23 +0700

    - init project
    Signed-off-by: Thanh Bui <thanhbpc@imt-soft.com>

---
+++ InterviewMecar/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InterviewMecar
+//
+//  Created by Cong Thanh on 3/19/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
